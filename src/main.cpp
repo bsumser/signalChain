f2c4dbf2d5739004ad519a5187812378dd5c7bc0
Brett Sumser <bsumser@gmail.com>
2021-08-11 16:24:38 -0700

    removed print for pedal data in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,5 @@ int main() {
 
     Pedal newPedal(effect, 11, 11, 11);
 
-    std::cout << newPedal.getEffect() << std::endl;
-    std::cout << newPedal.getVolume() << std::endl;
-    std::cout << newPedal.getTone() << std::endl;
-    std::cout << newPedal.getGain() << std::endl;
-
     return 0;
 }
