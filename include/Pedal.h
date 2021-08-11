3e09ea6dec0df8321f1da8ad14fc623a225c0719
Brett Sumser <bsumser@LAPTOP-SDV1MC09.localdomain>
2021-08-11 16:23:20 -0700

    Changed constructor to print information about type of pedal loaded

--- include/Pedal.h
+++ include/Pedal.h
@@ -2,16 +2,19 @@
 #define PEDAL_H
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 class Pedal
 {
     public:
         Pedal(std::string effectUser, int volumeUser, int toneUser, int gainUser);
+        Pedal(std::string effectUser, std::vector<int> subUnits, int volumeUser, int toneUser, int gainUser);
 		void setEffect(std::string value) { effect = value; }
 		void setVolume(int value) { volume = value; }
 		void setTone(int value) { tone = value; }
 		void setGain(int value) { gain = value; }
 		std::string getEffect() { return effect; }
+		void sendSignal() {;}
 		int getVolume() { return volume; }
 		int getTone() { return tone; }
 		int getGain() { return gain; }
--- src/Pedal.cpp
+++ src/Pedal.cpp
@@ -6,4 +6,10 @@ Pedal::Pedal(std::string effectUser, int volumeUser, int toneUser, int gainUser)
     volume = volumeUser;
     tone = toneUser;
     gain = gainUser;
+
+    std::cout << "Pedal loaded:" << std::endl <<
+    "effect = " << effect << std::endl <<
+    "volume = " << volume << std::endl <<
+    "tone = " << tone << std::endl <<
+    "gain = " << gain << std::endl;
 }
